b03ccc91dc7f78c2bb8b394de5fb1a27419398ea
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2025-02-04 23:03:06 +0530

    ui is ready now

---
+++ script.js
@@ -0,0 +1,25 @@
+// let playbutton = document.getElementById("playbutton");
+// let card = document.querySelectorAll("#scard1");
+// console.log(card)
+// Array.from(card).map((element)=>{
+//     element.addEventListener("mouseover", () => {
+//       playbutton.classList.add("showbtn");
+//       console.log(element,"hovered");
+//     });
+//     element.addEventListener("mouseout", () => {
+//       playbutton.classList.remove("showbtn");
+//       console.log("out");
+//     });
+// })
+// let group = document.getElementsByClassName("group");
+// console.log(group)
+// Array.from(group).forEach(element => {
+//     element.addEventListener("mouseover",()=>{
+//         playbutton.classList.add("showbtn");
+//         console.log("hovered")
+//     })
+// });
+// card.addEventListener("mouseout", () => {
+//     playbutton.classList.remove("showbtn");
+
+// });
