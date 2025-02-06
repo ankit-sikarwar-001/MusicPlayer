4f2e98b9110e16fc8b4d2f14383b36eb442410d8
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2025-02-06 23:08:12 +0530

    play button and player is working together now

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let pause = document.getElementById("pause");
+let play = document.getElementById("play");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
+
 async function getSongs() {
   let response = await fetch("http://127.0.0.1:5500/Songs/Popular_songs/");
   let a = await response.text();
@@ -13,86 +18,111 @@ async function getSongs() {
   }
   console.log(div);
   console.log(songs);
-  localStorage.setItem("titles",titles)
+  localStorage.setItem("titles", titles);
   return songs;
 }
+// let value = 0;
+let player = [];
+// localStorage.setItem("index", value);
 async function main() {
   let glory = await getSongs();
-  let player = [];
   glory.forEach((element) => {
     element = new Audio(element);
     player.push(element);
   });
-  let pause = document.getElementById("pause");
-  let play = document.getElementById("play");
-  let prev = document.getElementById("prev");
-  let next = document.getElementById("next");
-  let value = 0;
   let block = 1;
-  localStorage.setItem("index", value);
   play.addEventListener("click", () => {
-      let index = localStorage.getItem("index");
-      pause.style.display = "block";
-      play.style.display = "none";
-      player[index].play();
-      block = 0;
-    });
-    pause.addEventListener("click", () => {
-        let index = localStorage.getItem("index");
-        pause.style.display = "none";
-        play.style.display = "block";
-        player[index].pause();
-        block=1;
-    });
-    console.log(block);
-    
-  
-        prev.addEventListener("click", () => {
-            if(block == 1){
-            var index = localStorage.getItem("index");
-            index--;
-            if(index>=0)
-              localStorage.setItem("index", index);
-        }
-      });
-      next.addEventListener("click", () => {
-        if(block == 1){
-          let index = localStorage.getItem("index");
-          index++;
-          if(index<player.length)
-              localStorage.setItem("index", index);
-        }
-      });
-      let controller = document.getElementById("controller")
-      controller.addEventListener("click",()=>{
-        let store = localStorage.getItem("titles");
-        let storedTitle = Array.from(store.toString().split(","));
-        storedTitle = storedTitle.map((element) => element.replace(".mp3", ""));
-        console.log(storedTitle);
-        let index = localStorage.getItem("index");
-        let title = Array.from(document.querySelectorAll(".title"))
-        title = title.map(tit => tit.getAttribute("class"))
-        title = title.map((tit) => tit.split(" ")[0]);
-        console.log(title)
-        for(let i = 0 ;i<storedTitle.length;i++){
-          for(let j=0;j<title[i].length;j++){
-            if(title[i].charAt(j)===storedTitle[index].charAt(j)){
-              console.log(title[i]);
-             console.log(storedTitle[index].charAt(j));
-             document.querySelector(`.${title[i]}`).style.background =
-               "rgb(69, 187, 151)";
-          }
-          else{
-             document.querySelector(`.${title[i]}`).style.background = "";
+    let index = localStorage.getItem("index");
+    pause.style.display = "block";
+    play.style.display = "none";
+    player[index].play();
+    block = 0;
+  });
+  pause.addEventListener("click", () => {
+    let index = localStorage.getItem("index");
+    pause.style.display = "none";
+    play.style.display = "block";
+    player[index].pause();
+    block = 1;
+  });
+  console.log(block);
 
-          }
-        }
-        }
-        
-      })
-      
+  prev.addEventListener("click", () => {
+    if (block == 1) {
+      let index = localStorage.getItem("index");
+      if (index == 0) index = player.length - 1;
+      else index--;
+      if (index >= 0) localStorage.setItem("index", index);
     }
+  });
+  next.addEventListener("click", () => {
+    if (block == 1) {
+      let index = localStorage.getItem("index");
+      if (index == player.length - 1) index = 0;
+      else index++;
+      if (index < player.length) localStorage.setItem("index", index);
+    }
+  });
+  let controller = document.getElementById("controller");
+  let store = localStorage.getItem("titles");
+  let storedTitle = Array.from(store.toString().split(","));
+  storedTitle = storedTitle.map((element) => element.replace(".mp3", ""));
+  controller.addEventListener("click", () => {
+    // console.log(storedTitle);
+    let index = localStorage.getItem("index");
+    let title = Array.from(document.querySelectorAll(".title"));
+    title = title.map((tit) => tit.getAttribute("class"));
+    title = title.map((tit) => tit.split(" ")[0]);
+    console.log(title);
+    for (let i = 0; i < storedTitle.length; i++) {
+      if (title[i] === storedTitle[index]) {
+        document.querySelector(`.${title[i]}`).style.background =
+          "rgb(69, 187, 151)";
+      } else {
+        document.querySelector(`.${title[i]}`).style.background = "";
+      }
+    }
+  });
+  let playbtn = document.querySelectorAll("#playbutton");
+  // let shortTitle = storedTitle.map(element=> element.reduce(".mp3"," "))
+  console.log(storedTitle);
+  playbtn.forEach((element) => {
+    element.addEventListener("click", () => {
+      console.log(element.parentElement);
+      // let index = localStorage.getItem("index");
+      let title = element.parentElement.getAttribute("class");
+      title = title.split(" ")[0];
+      console.log(title);
+      for (let i = 0; i < storedTitle.length; i++) {
+        if (title == storedTitle[i]) {
+          document.querySelector(`.${storedTitle[i]}`).style.background =
+            "rgb(69, 187, 151)";
+          console.log(storedTitle[i]);
+          localStorage.setItem("index", i);
+          playe();
+          block = 0;
+        } else {
+          document.querySelector(`.${storedTitle[i]}`).style.background = "";
+        }
+      }
+    });
+  });
+}
+function playe() {
     
-  // console.log(title[0].innerText)
+    let index = localStorage.getItem("index");
+    pause.style.display = "block";
+    play.style.display = "none";
+    player.forEach((song,indi)=>{
+      if(index == indi){
+        song.play();
+      }
+      else{
+        song.pause();
+      }
+    })
+}
+
+// console.log(title[0].innerText)
 
 main();
